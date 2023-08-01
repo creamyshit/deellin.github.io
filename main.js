34d58699d1b7438844a98f15b99c9ec1fe46a779
Ulil <ulil.a@lif.id>
2023-08-01 08:52:06 +0700

    update website

--- main.js
+++ main.js
@@ -17,8 +17,8 @@
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+    document.getElementById("countdown").innerHTML = days + "days : " + hours + "hours : "
+    + minutes + "minutes : " + seconds + "seconds ";
 
     // If the count down is finished, write some text
     if (distance < 0) {
